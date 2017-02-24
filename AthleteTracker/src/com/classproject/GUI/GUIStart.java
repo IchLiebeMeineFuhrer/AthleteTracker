9828f2d145c40a38705f77ec9596353ebc0314b7
BrennanHurst <hurstbh11@gmail.com>
2017-02-24 14:43:22 -0500

    Athlete class

---
+++ AthleteTracker/src/com/classproject/GUI/GUIStart.java
@@ -0,0 +1,21 @@
+package com.classproject.GUI;
+
+import javax.swing.JApplet;
+
+public class GUIStart extends JApplet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 17431885152713844L;
+	
+	public GUIStart()
+	{
+		super();
+		this.setLayout(null);		
+		this.setVisible(true);
+
+	
+	}
+	
+}
