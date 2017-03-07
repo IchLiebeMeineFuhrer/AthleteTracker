24af60c11160489c74af69fd1d071ac7560bf810
brennanhurst <brennanhurst@10.3.160.177>
2017-03-07 11:51:04 -0500

    changed the configureLayout method

---
+++ AthleteTracker/src/com/classproject/GUI/components/GUIComponent.java
@@ -0,0 +1,33 @@
+package com.classproject.GUI.components;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+import com.classproject.GUI.GUIConstants;
+
+@SuppressWarnings("serial")
+public abstract class GUIComponent extends JPanel{
+
+
+	public GUIComponent()
+	{
+		this.configureLayout();
+	}
+	
+	
+	/**
+	 * Called at the start of this component, is used to set the layout of this component. 
+	 */
+	public void configureLayout(){
+		this.setBounds(0, 0, GUIConstants.SCREENWIDTH, GUIConstants.SCREENHEIGHT);
+		this.setBackground(Color.WHITE);
+		this.setVisible(true);
+	
+	};
+	
+	
+	
+	
+	
+}
